b4c9e5fbaa83adb960b99c13116f802ce49ed0d3
KOUDAGANI SAHITHYA <koudaganisahithya@KOUDAGANIs-MacBook-Air.local>
2026-05-28 16:26:48 +0530

    update backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       setLoading(true);
 
       const response = await axios.post(
-        "http://127.0.0.1:8001/analyze",
+        "https://ai-resume-analyzer-backend.onrender.com/analyze",
         formData
       );
 
